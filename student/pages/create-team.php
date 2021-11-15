6dc33e6771fe6ac13b35be1ffc790965cf93dd48
ankur-karn <67622196+ankur-karn@users.noreply.github.com>
2021-11-15 14:33:41 +0530

    Update create-team.php

--- student/pages/create-team.php
+++ student/pages/create-team.php
@@ -2,3 +2,22 @@
 include("../auth_session.php");
 include("../header.php");
 ?>
+
+        <h1>Enroll your Team as a Captain</h1>
+
+        <form action="create-team-sql.php" method="post">
+
+<p>
+                <label for="team_name">Team Name:</label>
+                <input type="text" name="team_name" id="team_name">
+            </p>
+<p>
+                <label for="solo_id">Your ID: </label>
+                <input type="text" name="solo_id" id="solo_id">
+            </p>
+
+            <input type="submit" value="Submit">
+        </form>
+</body>
+
+</html>
