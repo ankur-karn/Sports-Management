1f54e4a07daf3ff5d78dd10c459a4d52c85a4e83
ankur-karn <67622196+ankur-karn@users.noreply.github.com>
2021-11-15 14:40:03 +0530

    Update header.php

--- admin/header.php
+++ admin/header.php
@@ -12,13 +12,13 @@ table, th, td {
     <p>Logged in as admin <?php echo $_SESSION['username']; ?>&emsp;<a href="../logout-admin.php">logout</a></p>
     </div>
     <div>
-            <a href="sports-list.php">View sports list</a>&emsp;
-            <a href="category-list.php">View category list</a>&emsp;
-            <a href="members-list.php">View members list</a>&emsp;
-            <a href="team-list.php">View Team list</a>&emsp;
-            <a href="contestant-list.php">View Contestant list</a>&emsp;
+            <a href="sports-list.php">Sports list</a>&emsp;
+            <a href="category-list.php">Category list</a>&emsp;
+            <a href="members-list.php">Members list</a>&emsp;
+            <a href="team-list.php">Team list</a>&emsp;
+            <a href="contestant-list.php">Contestant list</a>&emsp;
             <a href="add-sport.php">Add Sports</a>&emsp;
-            <a href="add-category.php">Add category</a>&emsp;
+            <a href="add-category.php">Add Category</a>&emsp;
             <a href="add-event.php">Add event</a>&emsp;
             <a href="contestant-in-event.php">Add Contestant to event</a>&emsp;
     </div>
