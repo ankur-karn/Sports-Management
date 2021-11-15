857d9078aca81df14170cd267b942b01b6f9545b
ankur-karn <67622196+ankur-karn@users.noreply.github.com>
2021-11-15 11:22:07 +0530

    Update header.php

--- admin/header.php
+++ admin/header.php
@@ -9,17 +9,16 @@ table, th, td {
   <body>
     <div>IIITDM Jabalpur Sports Manager</div>
     <div>
-    <p>Logged in as admin <?php echo $_SESSION['username']; ?>!</p>
-    <p><a href="../logout-admin.php">Logout</a></p>
+    <p>Logged in as admin <?php echo $_SESSION['username']; ?>&emsp;<a href="../logout-admin.php">logout</a></p>
     </div>
     <div>
-            <a href="sports-list.php">View sports list</a><br>
-            <a href="category-list.php">View category list</a><br>
-            <a href="members-list.php">View members list</a><br>
-            <a href="team-list.php">View Team list</a><br>
-            <a href="contestant-list.php">View Contestant list</a><br>
-            <a href="add-sport.php">Add Sports</a><br>
-            <a href="add-category.php">Add category</a><br>
-            <a href="add-event.php">Add event</a><br>
-            <a href="contestant-in-event.php">Add Contestant to event</a><br>
+            <a href="sports-list.php">View sports list</a>&emsp;
+            <a href="category-list.php">View category list</a>&emsp;
+            <a href="members-list.php">View members list</a>&emsp;
+            <a href="team-list.php">View Team list</a>&emsp;
+            <a href="contestant-list.php">View Contestant list</a>&emsp;
+            <a href="add-sport.php">Add Sports</a>&emsp;
+            <a href="add-category.php">Add category</a>&emsp;
+            <a href="add-event.php">Add event</a>&emsp;
+            <a href="contestant-in-event.php">Add Contestant to event</a>&emsp;
     </div>
