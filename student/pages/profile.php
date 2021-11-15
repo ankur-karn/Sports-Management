c6028bf88df1a4d3043d55259241cb13a34108db
ankur-karn <67622196+ankur-karn@users.noreply.github.com>
2021-11-15 19:47:00 +0530

    Update profile.php

--- student/pages/profile.php
+++ student/pages/profile.php
@@ -10,7 +10,11 @@
       echo "Failed to connect to MySQL: " . mysqli_connect_error();
       }
 
+//------------------Profile------------------------------
     $result = mysqli_query($con,"SELECT * FROM member WHERE solo_id_roll = '{$_SESSION['username']}'");
+
+
+        echo "<h2>Your Profile Information:</h2>";
 echo "<table>";
 
 
@@ -27,6 +31,59 @@ echo "<table>";
       echo "<br />";
       }
  echo "</table>";
+
+//--------------------Contestant----------------------------
+
+
+      $result = mysqli_query($con,"SELECT * FROM contestant WHERE solo_id = '{$_SESSION['username']}'");
+          echo "<h2>You are a contestant in These Sports Categories</h2>";
+echo "<table>";
+
+    echo "  <tr>
+    <td>Your Contestant ID</td>
+    <td>Category ID</td>
+  </tr>";
+    while($row = mysqli_fetch_array($result))
+      {
+
+      echo "<tr><td>" . $row['contestant_id']. "</td><td> " . $row['category_id'].  "</td></tr> ";
+      echo "<br />";
+      }
+ echo "</table>";
+//----------------------Captain---------------------------
+    $result = mysqli_query($con,"SELECT * FROM team WHERE captain_id = '{$_SESSION['username']}'");
+
+    echo "<table>";
+    echo "<br><h2>You are a captain of These teams:</h2>";
+    echo "  <tr>
+    <td>Team ID</td>
+    <td>Team Name</td>
+  </tr>";
+    while($row = mysqli_fetch_array($result))
+      {
+      echo "<tr><td>" . $row['team_id']. "</td><td> " . $row['team_name']. "</td></tr> ";
+      echo "<br />";
+      }
+ echo "</table>";
+
+ //---------------------Events----------------------------
+     $result = mysqli_query($con,"SELECT * FROM contestant_in_event JOIN contestant ON  contestant_in_event.contestant_id= contestant.contestant_id AND contestant.solo_id = '{$_SESSION['username']}'");
+
+    echo "<table>";
+    echo "<br><h2>Your Upcoming Events:</h2>";
+    echo "  <tr>
+    <td>Event ID</td>
+    <td>Contestant ID</td>
+  </tr>";
+    while($row = mysqli_fetch_array($result))
+      {
+      echo "<tr><td>" . $row['event_id']. "</td><td> " . $row['contestant_id']. "</td></tr> ";
+      echo "<br />";
+      }
+ echo "</table>";
+
+
+
     mysqli_close($con);
     ?>
 
