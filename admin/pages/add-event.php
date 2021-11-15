ac28627663b4b950eadaa9f344de3aadfe3441c3
ankur-karn <67622196+ankur-karn@users.noreply.github.com>
2021-11-15 10:17:51 +0530

    Create add-event.php

---
+++ admin/pages/add-event.php
@@ -0,0 +1,22 @@
+<?php
+    include("../auth_session.php");
+    include("../header.php");
+?>
+        <h1>Add event</h1>
+
+        <form action="add-event-sql.php" method="post">
+
+<p>
+                <label for="event_name">Event Name:</label>
+                <input type="text" name="event_name" id="event_name">
+            </p>
+<p>
+                <label for="event_day">Event day: </label>
+                <input type="text" name="event_day" id="event_day">
+            </p>
+
+            <input type="submit" value="Submit">
+        </form>
+</body>
+
+</html>
