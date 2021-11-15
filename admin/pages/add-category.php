30c9bbb8c295afaba8c7b7333a5d45a543a75691
ankur-karn <67622196+ankur-karn@users.noreply.github.com>
2021-11-15 11:27:29 +0530

    Update add-category.php

--- admin/pages/add-category.php
+++ admin/pages/add-category.php
@@ -2,22 +2,22 @@
     include("../auth_session.php");
     include("../header.php");
 ?>
-        <h1>Add event</h1>
+        <h1>Add Category</h1>
 
         <form action="add-category-sql.php" method="post">
 
 
           <label for="sport_name"><b>Sport Name(already in list)</b></label>
-          <input type="text" placeholder="Enter Sport's name" name="sname" required />
+          <input type="text" placeholder="Enter Sport's name" name="sname" required /><br>
 
           <label for="category_name"><b>New Category name</b></label>
-          <input type="text" placeholder="Enter new category name" name="cname" required />
+          <input type="text" placeholder="Enter new category name" name="cname" required /><br>
 
           <label for="gender"><b>Gender</b></label>
-          <input type="text" placeholder="male or female" name="gender" required />
+          <input type="text" placeholder="male or female" name="gender" required /><br>
 
           <label for="team_size"><b>Team Size (1 for solo)</b></label>
-          <input type="text" placeholder="Numerical value" name="tsize" required />
+          <input type="text" placeholder="Numerical value" name="tsize" required /><br>
 
             <input type="submit" value="Submit">
         </form>
