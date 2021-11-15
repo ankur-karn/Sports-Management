2ca45e46f07d38ac5dfb82f5327ca4ac4f570367
ankur-karn <67622196+ankur-karn@users.noreply.github.com>
2021-11-15 10:59:20 +0530

    Add files via upload

---
+++ admin/pages/team-list.php
@@ -0,0 +1,32 @@
+<?php
+
+    include("../auth_session.php");
+    include("../header.php");
+
+    $con=mysqli_connect("localhost","root","","sports");
+    // Check connection
+    if (mysqli_connect_errno())
+      {
+      echo "Failed to connect to MySQL: " . mysqli_connect_error();
+      }
+
+    $result = mysqli_query($con,"SELECT * FROM team");
+echo "<table>";
+
+    echo "  <tr>
+    <td>Team ID</td>
+    <td>Team Name</td>
+
+  </tr>";
+    while($row = mysqli_fetch_array($result))
+      {
+
+      echo "<tr><td>" . $row['team_id']. "</td><td> " . $row['team_name']. "</td></tr> "; //these are the fields that you have stored in your database table employee
+      echo "<br />";
+      }
+ echo "</table>";
+    mysqli_close($con);
+    ?>
+
+    </div>
+</html>
